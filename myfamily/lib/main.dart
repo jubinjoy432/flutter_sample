fdcb509aeabc2336f394bb1dece2b63d233abbbc
Jubin Joy <jubinjoy432@gmail.com>
2021-09-30 17:28:03 +0530

    Second Task Completed

--- myfamily/lib/main.dart
+++ myfamily/lib/main.dart
@@ -34,42 +34,214 @@ class _homeState extends State<home> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              height: 100,
-              child: Text(
-                "Joy Mathew",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => Joy()));
+                    },
+                    child: Text("Joy Mathew",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ))),
               ),
             ),
-            Container(
-              height: 100,
-              child: Text(
-                "Jeena Joy",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Jeena()));
+                    },
+                    child: Text("Jeena Joy",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ))),
               ),
             ),
-            Container(
-              height: 100,
-              child: Text(
-                "Juhi Joy",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Juhi()));
+                    },
+                    child: Text("Juhi Joy",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ))),
               ),
             ),
-            Container(
-              height: 100,
-              child: Text(
-                "Jubin Joy",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Jubin()));
+                    },
+                    child: Text("Jubin Joy",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ))),
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Joy extends StatefulWidget {
+  const Joy({Key? key}) : super(key: key);
+
+  @override
+  _JoyState createState() => _JoyState();
+}
+
+class _JoyState extends State<Joy> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Joy Mathew"),
+      ),
+      body: Container(
+        height: 1000,
+        width: 1000,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                child: Text(
+              "Name: Joy Mathew",
+              style: TextStyle(fontSize: 30),
+            )),
+            Container(
+              child: Text("Occupation: Govt.Service",
+                  style: TextStyle(fontSize: 30)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Jeena extends StatefulWidget {
+  const Jeena({Key? key}) : super(key: key);
+
+  @override
+  _JeenaState createState() => _JeenaState();
+}
+
+class _JeenaState extends State<Jeena> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jeena Joy"),
+      ),
+      body: Container(
+        height: 1000,
+        width: 1000,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                child: Text(
+              "Name: Jeena Joy",
+              style: TextStyle(fontSize: 30),
+            )),
+            Container(
+              child: Text("Occupation: House Wife",
+                  style: TextStyle(fontSize: 30)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Juhi extends StatefulWidget {
+  const Juhi({Key? key}) : super(key: key);
+
+  @override
+  _JuhiState createState() => _JuhiState();
+}
+
+class _JuhiState extends State<Juhi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Juhi Joy"),
+      ),
+      body: Container(
+        height: 1000,
+        width: 1000,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                child: Text(
+              "Name: Juhi Joy",
+              style: TextStyle(fontSize: 30),
+            )),
+            Container(
+              child:
+                  Text("Occupation: QA Tester", style: TextStyle(fontSize: 30)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Jubin extends StatefulWidget {
+  const Jubin({Key? key}) : super(key: key);
+
+  @override
+  _JubinState createState() => _JubinState();
+}
+
+class _JubinState extends State<Jubin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jubin Joy"),
+      ),
+      body: Container(
+        height: 1000,
+        width: 1000,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                child: Text(
+              "Name: Jubin Joy",
+              style: TextStyle(fontSize: 30),
+            )),
+            Container(
+              child:
+                  Text("Occupation: Student", style: TextStyle(fontSize: 30)),
+            )
           ],
         ),
       ),
